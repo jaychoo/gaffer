c5652ab2759d91f668d912e7e9177fb4af8bb918
Jay Choo <jaychoo@gmail.com>
2012-05-05 01:58:42 -0400

    load python modules from /modulues directory

--- gaffer/api/loader.py
+++ gaffer/api/loader.py
@@ -1,2 +1,24 @@
+import os
+import sys
+import re
 
+DEFAULT_PATH = './modules'
+INVALID = ('~', '__init__', '.pyc')
+
+
+def get_modules(path=DEFAULT_PATH):
+    regex = re.compile(r'~|__init__|pyc')
+    # Generator chain
+    items = (i for i in os.listdir(path))
+    filenames = (i for i in items if not regex.search(i))
+    modules = map(lambda j: j[0], map(os.path.splitext, filenames))
+    return set(modules)
+
+if __name__ == '__main__':
+    modules = get_modules()
+    sys.path.insert(0, 'modules')
+
+    for m in modules:
+        mod = __import__(m)
+        mod.run()
 
